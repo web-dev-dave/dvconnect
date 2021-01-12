74ca157dcbc38d1ab69cd356cb599a871f1f2585
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-01-12 18:57:50 +1300

    api running

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const server = express()
 
+server.get('/', (req, res) => {
+  res.send('API Running')
+})
+
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
