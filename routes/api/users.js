ed20293a9cc6d394b5d7a9b927e20b4de0e3b264
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-01-12 22:19:14 +1300

    can register user to database and can detect if email is already present

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
 const { body, validationResult } = require('express-validator')
+
+const User = require('../../models/User')
 
 // @route     POST api/users
 // @desc      Register user
@@ -13,22 +17,57 @@ router.post(
   // email must be an email
   body('email', 'Please include a valid email').isEmail(),
   // password must be at least 5 chars long
-  body('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),(req, res) => {
+  body('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+  async (req, res) => {
     // Finds the validation errors in this request and wraps them in an object with handy functions
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res
+        .status(400)
+        .json({ errors: errors.array() });
     }
-
-    // See if user exists
-
-    // Get users gravatar
-
-    // Encrypt password
-
-    // Return jsonwebtoken
     
-    res.send('User route')
+    const { name, email, password} = req.body
+
+    try{
+      // See if user exists
+      let user = await User.findOne({ email })
+
+      if(user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }]})
+      }
+
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      })
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      })
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10)
+
+      user.password = await bcrypt.hash(password, salt)
+
+      await user.save()
+  
+      // Return jsonwebtoken
+  
+      res.send('User registered')
+
+    } catch(err) {
+      console.error(err.message)
+      res.status(500).send('Server error')
+    }
 })
 
 module.exports = router
