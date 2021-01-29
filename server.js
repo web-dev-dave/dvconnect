abee6e939e9e008c89b9586e25c829c183c37492
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-01-29 14:22:50 +1300

    bring react in for front end

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ server.get('/', (req, res) => {
   res.send('API Running');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log('Listening on port', PORT);
