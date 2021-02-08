5a9701b33213d164747ed7e21dc256bee0652f1d
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-02-09 11:17:35 +1300

    profile reducer and get current profile

---
+++ client/src/actions/profile.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { setAlert } from './alert';
+
+import { GET_PROFILE, PROFILE_ERROR } from './types';
+
+// Get current users profile
+export const getCurrentProfile = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/profile/me');
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (error) {
+    const errors = error.response.statusText;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
