5fbec79b354efc10136219602eabc4c99c7f70cb
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-02-10 11:57:58 +1300

    create add experience component

---
+++ client/src/components/profile-forms/AddExperience.js
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addExperience } from '../../actions/profile';
+
+const AddExperience = ({ addExperience, history }) => {
+  const [formData, setFormData] = useState({
+    title: '',
+    company: '',
+    location: '',
+    from: '',
+    to: '',
+    current: '',
+    description: ''
+  });
+
+  const { title, company, location, from, to, current, description } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    addExperience(formData, history);
+  };
+
+  return (
+    <>
+      <h1 className='large text-primary'>Add An Experience</h1>
+      <p className='lead'>
+        <i className='fas fa-code-branch'></i> Add any developer/programming
+        positions that you have had in the past
+      </p>
+      <small>* = required field</small>
+      <form className='form' onClick={onSubmit}>
+        <div className='form-group'>
+          <input
+            type='text'
+            value={title}
+            onChange={onChange}
+            placeholder='* Job Title'
+            name='title'
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            value={company}
+            onChange={onChange}
+            placeholder='* Company'
+            name='company'
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            value={location}
+            onChange={onChange}
+            placeholder='Location'
+            name='location'
+          />
+        </div>
+        <div className='form-group'>
+          <h4>From Date</h4>
+          <input type='date' value={from} onChange={onChange} name='from' />
+        </div>
+        <div className='form-group'>
+          <p>
+            <input
+              type='checkbox'
+              value={current}
+              onChange={onChange}
+              name='current'
+            />{' '}
+            Current Job
+          </p>
+        </div>
+        <div className='form-group'>
+          <h4>To Date</h4>
+          <input type='date' value={to} onChange={onChange} name='to' />
+        </div>
+        <div className='form-group'>
+          <textarea
+            name='description'
+            cols='30'
+            rows='5'
+            value={description}
+            onChange={onChange}
+            placeholder='Job Description'
+          ></textarea>
+        </div>
+        <input type='submit' className='btn btn-primary my-1' />
+        <Link className='btn btn-light my-1' to='/dashboard'>
+          Go Back
+        </Link>
+      </form>
+    </>
+  );
+};
+
+AddExperience.propTypes = {
+  addExperience: PropTypes.func.isRequired
+};
+
+export default connect(null, { addExperience })(withRouter(AddExperience));
