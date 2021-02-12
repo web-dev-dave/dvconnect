55cdc572476d32a3e50e319589fa9c40d2e07934
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-02-13 12:49:00 +1300

    post item component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,17 +1,36 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'react-moment';
 import { connect } from 'react-redux';
 
 // Actions and helpers
 import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return <div>Posts</div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <>
+      <h1 class='large text-primary'>Posts</h1>
+      <p class='lead'>
+        <i class='fas fa-user'></i> Welcome to the community!
+      </p>
+
+      {/* PostForm */}
+
+      <div class='posts'>
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 Posts.propTypes = {
